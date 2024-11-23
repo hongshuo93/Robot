411f5f0118b4d79905e28c79abfc0e27dd2ac8b8
hongshuo <hongshuo.sun@decagames.cn>
2024-11-23 17:43:21 +0800

    init 2

--- robot.cpp
+++ robot.cpp
@@ -10,16 +10,26 @@
 #include <Kenblock_4DigitalDisplay.h>
 #include <Kenblock_Buzzer.h>
  
-uint8_t keyScan(int pin) {
-    int value = analogRead(pin);       
-   // int yello_1 = 104;
-   // int white_2 = 225;
-   // int red_3 = 13;
-   // int blue_4 = 197;
-   // int green_5 = 90;
-    int numArray[6]={250,104,225,13,197,90};
-    return numArray[1];
 
+uint8_t keyScan(int pin) {
+    uint8_t value = analogRead(pin);       
+
+    int numArray[6] = {250, 104, 225, 13, 197, 90}; // 按钮对应的电压值
+    for (int idx = 0; idx < 6; idx++) { // 修正循环范围，数组索引从0开始
+        int absDiff = abs(value - numArray[idx]); // 计算绝对差值
+        // 优化打印信息，减少内存使用
+       /* Serial.print("[");
+        Serial.print(idx);
+        Serial.print("]: ");
+        Serial.print(numArray[idx]);
+        Serial.print(" value: ");
+        Serial.println(value);*/
+        
+        if (absDiff < 5) { // 如果差值小于阈值，则返回按键索引
+            return idx; // 按键编号从1开始
+        }
+    }
+    return 0; // 如果没有匹配值，返回0
 }
 // 按钮防抖函数
 uint8_t debounceButton(int buttonPin, unsigned long debounceDelay = 50) {
@@ -50,15 +60,57 @@ uint8_t debounceButton(int buttonPin, unsigned long debounceDelay = 50) {
 
 
 FourDigitalDisplay disp_0(PD1);
-uint8_t passwd = 0;
+int passwd = 0;
 int keyMatrix_0 = A0;
-
+int led_blue = 8;
+int led_yello = 9;
+int led_red = 10;
 void setup() {
     passwd = 0;
+    Serial.begin(9600);
     pinMode(keyMatrix_0, INPUT_PULLUP);
+    pinMode(led_blue, OUTPUT);
+    pinMode(led_yello, OUTPUT);
+    pinMode(led_red, OUTPUT);
+}
+uint8_t correctPasswd =1234;
+uint8_t errorCnt = 0;
+void loop() {
+
+    int num = debounceButton(keyMatrix_0);
+    if(num>0){
+        passwd = passwd*10+num;
+        Serial.print("num: ");
+        Serial.print(num);
+        Serial.print(" passwd: ");
+        Serial.print(passwd);
+        Serial.println();
+    }
+    if(passwd>10000||passwd<0){
+        passwd = 0;
+    }
+    if(passwd == correctPasswd){
+        digitalWrite(led_blue, HIGH);
+        digitalWrite(led_yello, LOW);
+        digitalWrite(led_red, LOW);
+        errorCnt = 0;
+    }else if(passwd>1000){
+        errorCnt = errorCnt+1;
+        if(errorCnt>=3){
+            digitalWrite(led_blue, LOW);
+            digitalWrite(led_yello, LOW);
+            digitalWrite(led_red, HIGH);
+        }else{
+            digitalWrite(led_blue, LOW);
+            digitalWrite(led_yello, HIGH);
+            digitalWrite(led_red, LOW);
+        }
+    }else{
+        digitalWrite(led_blue, LOW);
+        digitalWrite(led_yello, LOW);
+        digitalWrite(led_red, LOW);
+    }
+
+    disp_0.display(passwd);
 }
 
-void loop() {
-    int num = keyScan(keyMatrix_0);
-    disp_0.display(num);
-}
